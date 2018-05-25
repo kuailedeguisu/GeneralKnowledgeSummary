1c9c95adaf1012b6e00f443c6a0c8eb18303aed8
lch <34417498+kuailedeguisu@users.noreply.github.com>
2018-05-25 14:37:45 +0800

    debugHack source code
    Update installation.txt

--- debugHack/coreDumpExa.cpp
+++ debugHack/coreDumpExa.cpp
@@ -1,15 +1,28 @@
+<<<<<<< HEAD
 #include <stdio.h>
 
 int max(const int& a, const int& b)
 {
     int* pInt = NULL;
     printf("%d",*pInt);
+=======
+#include <iostream>
+using namespace std;
+int max(const int& a, const int& b)
+{
+    int* pInt = NULL;
+    cout << *pInt << endl;
+>>>>>>> debugHack source code
     return a > b ? a : b;
 }
 
 int main()
 {
     int a = 5, b = 6;
+<<<<<<< HEAD
     printf("%d", max(a,b));
+=======
+    cout << max(a,b) << endl;
+>>>>>>> debugHack source code
     return 0;
 }
