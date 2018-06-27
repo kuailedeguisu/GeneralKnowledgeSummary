5e9f09190225fba93f880c3ce1a2e20f09da4de5
Chunhong <liuchunhong522@163.com>
2018-06-27 01:12:44 -0400

    commit Sales_item

--- CPlusPlus/cplusplusprimer5/ch01/Sales_item.cpp
+++ CPlusPlus/cplusplusprimer5/ch01/Sales_item.cpp
@@ -1,117 +1,5 @@
 #include "Sales_item.hpp"
 
-/*Sales_item::Sales_item(
-    std::string isbn,
-    float perPrice,
-    unsigned nums
-):
-    myIsbn(isbn),
-    myPerPrice(perPrice),
-    myNums(nums)
-{
-
-}
-
-Sales_item::Sales_item(const Sales_item& src)
-{
-    myIsbn = src.getIsbn();
-    myNums = src.getNums();
-    myPerPrice = src.getPerPrice();
-}
-
-bool Sales_item::operator==(const Sales_item& src)
-{
-    return myIsbn == src.getIsbn() &&
-        myPerPrice == src.getPerPrice() &&
-        myNums == src.getNums();
-}
-
-Sales_item& Sales_item::operator=(const Sales_item& src, Sales_item& dest)
-{
-    if (dest == src)
-        return *this;
-
-    this->myIsbn = src.getIsbn();
-    this->myPerPrice = src.getPerPrice();
-    this->myNums = src.getNums();
-
-    return *this;
-}
-
-Sales_item::~Sales_item()
-{
-
-}
-
-Sales_item& Sales_item::operator+(const Sales_item& item1, const Sales_item& item2)
-{
-    Sales_item tmpItem("", 0, 0.0);
-
-    if (item1.getIsbn() == item2.getIsbn())
-    {
-        tmpItem.myIsbn = item1.getIsbn();
-        tmpItem.myNums = item1.getNums() + item2.getNums();
-        tmpItem.myPerPrice = (computeTotalPrice(item1) + computeTotalPrice(item2)) / tmpItem.myNums; 
-    }
-    else
-    {
-        std::cout << "They can not be added because they have different isbn." << std::endl;
-    }
-
-    return tmpItem;
-}
-
-float Sales_item::computeTotalPrice(const Sales_item& item)
-{
-    return item.getNums() * item.getPerPrice();
-}
-
-std::string Sales_item::getIsbn() const
-{
-    return myIsbn;
-}
-
-void Sales_item::setIsbn(std::string isbn)
-{
-    myIsbn = isbn;
-}
-
-int Sales_item::getNums() const
-{
-    return myNums;
-}
-
-void Sales_item::setNums(int nums)
-{
-    myNums = nums;
-}
-    
-void Sales_item::setPerPrice(float perPrice)
-{
-    myPerPrice = perPrice;
-}
-
-float Sales_item::getPerPrice() const
-{
-    return myPerPrice;
-}
-
-int main()
-{
-    Sales_item item1("0-201-899-0", 5.0, 2);//construction function
-    Sales_item item2("0-201-899-0", 6.5, 3);
-    Sales_item item3(item1);//copy construction function
-    Sales_item item4 = item1;//assignment operator
-    Sales_item item5("0-901", 4.0, 3);
-    Sales_item item6 = item1 + item2;
-
-    std::cout << "item1 + item2:" << item6.getPerPrice() << std::endl;//operator + with the same isbn
-    std::cout << "item4:" << item4.getPerPrice() << std::endl;
-    //std::cout << "item1 + item5:" << item1 + item5 << std::endl;//operator + with different isbn
-
-    return 0;
-}*/
-
 inline bool isSameIsbn(const Sales_item& lhs, const Sales_item& rhs)
 {
     return lhs.getIsbn() == rhs.getIsbn();
@@ -227,5 +115,40 @@ int main()
     std::cout << "avg_price for item1:" << item1.avg_price() << std::endl;
     std::cout << "avg_price for item1 + item2:" << item3.avg_price() << std::endl;
 
+    //count all sold numbers
+    
+    Sales_item total;
+    if (std::cin >> total)
+    {
+        Sales_item trans;
+
+        while (std::cin >> trans)
+        {
+            if (trans.getIsbn() == total.getIsbn())
+            {
+                total += trans;
+            }
+            else
+            {
+                std::cout << total << std::endl;
+                total = trans;
+            }
+        }
+
+        std::cout << total << std::endl;
+
+    }
+    else
+    {
+        std::cout << "No data?" << std::endl;
+        return -1;
+    }
+
     return 0;
 }
+
+/*
+the result is strange:
+for count all sold numbers,
+it should have empty isbn, so we should enter isbn, but it needed?
+*/
